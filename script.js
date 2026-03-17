5ac210c8f3e8fa840873c4f186770d6e416ec4dc
Piotr2025 <psobczy@student.agh.edu.pl>
2026-03-17 01:47:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-const navbar = document.getElementById('navbar');
-const progressBar = document.getElementById('progress-bar');
-const filler = document.getElementById('filler-text');
+// Wybieramy elementy z HTML
+const progressBar = document.getElementById("myBar");
+const navbar = document.getElementById("navbar");
+const widthDisplay = document.getElementById("window-width");
+const textBox = document.getElementById("text-box");
 
-// Generujemy tekst, żeby było co przewijać
-for(let i=0; i<30; i++) {
-    filler.innerHTML += `<p>To jest przykładowy akapit numer ${i+1}. OSPF to protokół routingu typu link-state...</p>`;
+// 1. Generujemy dużo tekstu, żeby strona była długa (dla celów demo)
+for(let i = 1; i <= 50; i++) {
+    textBox.innerHTML += `<h3>Akapit nr ${i}</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>`;
 }
 
-// Główna funkcja obsługująca scroll
-window.addEventListener('scroll', () => {
-    // 1. Logika paska postępu
-    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
+// 2. Obsługa zdarzenia SCROLL
+window.onscroll = function() {
+    handleScroll();
+};
+
+function handleScroll() {
+    // Obliczamy o ile pikseli użytkownik zjechał w dół
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     
+    // Obliczamy całkowitą wysokość strony minus wysokość okna
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    
+    // Obliczamy procent (Matematyka!)
+    let scrolled = (winScroll / height) * 100;
+    
+    // Ustawiamy szerokość paska
     progressBar.style.width = scrolled + "%";
 
-    // 2. Logika zmniejszania menu (powyżej 100px)
+    // Zmniejszanie menu po 100px
     if (winScroll > 100) {
-        navbar.classList.add('small-menu');
+        navbar.classList.add("small-menu");
     } else {
-        navbar.classList.remove('small-menu');
+        navbar.classList.remove("small-menu");
     }
-});
+}
 
-// Obsługa zdarzenia resize (pokazanie szerokości okna)
-window.addEventListener('resize', () => {
-    console.log("Szerokość okna: " + window.innerWidth);
-    // Tutaj można dodać np. zmianę koloru tła przy zmianie rozmiaru
-});
+// 3. Obsługa zdarzenia RESIZE
+window.onresize = function() {
+    // Wyświetlamy aktualną szerokość okna
+    widthDisplay.innerText = window.innerWidth;
+};
+
+// Wywołujemy raz na starcie, żeby wpisać początkową szerokość
+widthDisplay.innerText = window.innerWidth;
